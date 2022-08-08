45148b07bdacaa4878ea01d78c92edcfed469dcf
MIchael <tmkovver@mail.ru>
2022-08-08 22:49:15 +0700

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let output = str.split(''); // разбиваем строку на массив из символов
+
+  for (let i = 0; i < output.length; i++) {
+    bracketsConfig.forEach((element) => {
+      if (output[i] === element[0] && output[i + 1] === element[1]) { // сравниваем и удаляем по два элемента из массива, начиная с индекса i, после чего корректируем i
+        output.splice(i, 2);
+        i -= 2;
+      }
+    });
+  }
+
+  if (output.length === 0) { // если ничего лишнего не осталось, то выводим true
+    return true
+  } else {
+    return false
+  }
 }
